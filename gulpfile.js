879de8c67db67c5bcbfb334f72f953060560026f
snake <122012225@qq.com>
2016-06-13 15:47:19 +0800

    2016/6/13 gupfile.js 修改 package.json添加

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,44 @@
-var gulp            = require('gulp');
-var concat          = require('gulp-concat');
-var sass            = require('gulp-ruby-sass');
-var watch           = require('gulp-watch');
-var autoprefixer    = require('gulp-autoprefixer');
-var csscombo        = require(('gulp-css-combo'));
-var convertEncoding = require('fd-gulp-convert-encoding');
-var post            = require('gulp-postCSS');
+var gulp         = require('gulp'),
+    sass         = require('gulp-ruby-sass'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'), 
+    concat       = require('gulp-concat'),
+    rename       = require('gulp-rename'),
+    uglify       = require('gulp-uglify'),
+    cssmin       = require('gulp-minify-css'),
+    notify       = require('gulp-notify'),
+    imagemin     = require('gulp-imagemin');
 
-gulp.task('watch', function () {
-    gulp.watch('../gbicom/css/common/*.css', ['concatcss']);
-    gulp.watch('../gbicom/css/pages/*.css', ['concatcss']);
+//样式处理 sass编译，autoprefixer添加前缀，
+gulp.task('sass', () =>
+    sass('./sass/base.scss', {sourcemap: true})
+        .on('error', sass.logError)
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer({
+            browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
+            cascade: true,  //美化属性
+            remove:true     //是否去掉不必要的前缀 默认：true 
+            }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'))
+        .pipe(notify({ message: 'Sass task complete' }));
+);
+
+//样式压缩后重命名输出 
+gulp.task('cssmin', function () {
+    gulp.src('./dist/base.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(notify({ message: 'Styles task complete' }));
+
 });
 
-gulp.task('sass', function() {
-    return sass('sass/') 
-    .on('error', function (err) {
-      console.error('Error!', err.message);
-   })
-    .pipe(gulp.dest('dist/css'))
+gulp.task('images', function() {
+  return gulp.src('./images/**/*')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('dist/assets/img'))
+    .pipe(notify({ message: 'Images task complete' }));
 });
 
-gulp.task('post', function () {
-  var processors = [
-  ];
-  return gulp.src('./src/*.css')
-    .pipe(postcss(processors))
-    .pipe(gulp.dest('./dest'));
-});
-
-gulp.task('combo', function () {
-    return gulp.src('../gbicom/css/base.css')
-        .pipe(csscombo())
-        .pipe(convertEncoding('utf-8'))
-        .pipe(gulp.dest('../gbicom/css/base.min.css'));
-});
-
-gulp.task('default', ['post']);
+gulp.task('default', ['sass']);
