6d3dcb5163ef5207f264c73ae668ac0d5318769c
yaoyulong <122012225@qq.com>
2023-06-12 20:30:14 +0800

    axios hooks

--- vitets/src/views/index/index.vue
+++ vitets/src/views/index/index.vue
@@ -7,6 +7,21 @@
 <script setup>
 import {phoneFn, thrFn , deFn } from '@/assets/js/util'
 console.log(phoneFn(13333333333));
+import axios from '@/hooks/axios'
+
+const {$post, $postt} = axios()
+
+let url = `/Common/getcodenew_wx`
+let data = {
+  mobile: 13333333333,
+  check: 1,
+  code: 321456
+}
+$post(url, data).then( res => {
+  console.log(res);
+})
+
+
 
 </script>
 
