324e5e66299fa0a0e6c6dbfbe88b242212ce25a1
yaoyulong <yaoyulong@ciprun.com>
2023-10-20 17:10:20 +0800

    跟进管理接口联调 客户管理列表接口联调，联系人更换所属人接口联调等

--- js-ts/js.js
+++ js-ts/js.js
@@ -1,1 +1,10 @@
 console.log(123);
+window.addEventListener('beforeunload', e => beforeunloadHandler(e))
+  
+  function beforeunloadHandler (e) {
+    let box = document.querySelector('.close')
+    box.style.display = 'block'
+    e.preventDefault()
+    e.returnValue = '123123'
+    return false;
+  }
