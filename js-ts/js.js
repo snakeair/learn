af95ebb61a658360979b1c03721ad3c961ea31ba
yaoyulong <122012225@qq.com>
2023-12-25 08:46:31 +0800

    up

--- js-ts/js.js
+++ js-ts/js.js
@@ -1,1 +1,28 @@
-console.log(123);
+let type = {
+  'job' :0,
+  'gongjijin':0 ,
+  'house' :0,
+  'vehicle' :0,
+  'shebao' :0,
+  'insurance':0 ,
+  'overdue':3 ,
+}
+let checkType = {
+  'job' :1,
+  'gongjijin':2 ,
+  'house' :3,
+  'vehicle' :3,
+  'shebao' :2,
+  'insurance':2 ,
+  'overdue':0 ,
+}
+
+
+let check = ['job', 'vehicle', 'insurance']
+
+for(let key in checkType) {
+  check.includes(key) ? type[key] = checkType[key] : ''
+}
+
+let [...check] = checkType
+console.log(check);
